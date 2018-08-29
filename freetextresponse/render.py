7473fa1c40d51bc408ba2787d09e670da59bf646
1M4 <imanol.cobian@teltek.es>
2018-08-29 10:35:07 +0200

    Deleted not used function

--- freetextresponse/render.py
+++ freetextresponse/render.py
@@ -2,13 +2,6 @@ import pkg_resources
 
 from django.template import Template
 
-def resource_string(self, path):
-    """Handy helper for getting resources from our kit."""
-    data = pkg_resources.resource_string(__name__, path)
-
-    return data.decode("utf8")
-
-
 def load_resource(resource_path):
     """
     Gets the content of a resource
